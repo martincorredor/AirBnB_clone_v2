ba4b9cbaf79622696ff380bb9ea141daf98cf211
martincorredor <dev.martincorredor@gmail.com>
2021-03-29 12:33:20 -0500

    [ADDED] Creates an user on the database hbnb_test_db on MySQL server

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
--- prepares a MySQL server for the project
--- create the DB, an user 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+-- script that prepares a MySQL server for the project
+-- Creates a database and a user with a password
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
