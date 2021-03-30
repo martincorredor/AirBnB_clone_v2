d79e25d4c27f863db6471dcdcab725d8e1a84aad
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-30 11:09:14 -0500

    [UPDATE] - Adding Column to attributes

--- models/user.py
+++ models/user.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
+    __tablenanme__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
     email = ''
     password = ''
     first_name = ''
