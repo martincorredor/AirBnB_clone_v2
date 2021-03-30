c8dcfc3fec9442e5116d6dddcea1ed0baf161aeb
martincorredor <dev.martincorredor@gmail.com>
2021-03-30 16:00:56 -0500

    [UPDATE] Link the class State with City model

--- models/state.py
+++ models/state.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from file_storage import FileStorage
+from models.engine.file_storage import FileStorage
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref='state',
+
                             cascade="all, delete, delete-orphan")
 
+if env("HBNB_TYPE_STORAGE") != 'db':
     @property
     def cities(self):
         """
