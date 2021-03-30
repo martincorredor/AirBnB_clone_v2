93c3ee16ffa38f7a3d66da75a5649ffae151b85e
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-30 14:25:22 -0500

    [UPDATE] - Add relationship and property getter attribute

--- models/state.py
+++ models/state.py
@@ -2,9 +2,25 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from file_storage import FileStorage
 
 
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                            cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        Returns the list of City instances with state_id equals to the current State.id
+        """
+        new_list = []
+        instances = FileStorage.all()
+        for key, value in (instances):
+            if value.state_id == State.id:
+                new_list.append(value)
+        return new_list
