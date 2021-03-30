28f83ce391af3b275d9563a6fdd2066a41f1da30
martincorredor <dev.martincorredor@gmail.com>
2021-03-30 15:58:56 -0500

    [UPDATE] Add scripts to link the modules in the folder

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv as env
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.state import State
+from models.user import User
+
 
 
 if env("HBNB_TYPE_STORAGE") == 'db':
