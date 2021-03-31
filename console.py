025628f6d84e3c53eb3c723c2e977ff6b883bffd
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-30 22:30:29 -0500

    [UPDATE] - delete line 152 for create new instance

--- console.py
+++ console.py
@@ -149,7 +149,6 @@ class HBNBCommand(cmd.Cmd):
             setattr(new_instance, argument[0], argument[1])
 
         print(new_instance.id)
-        storage.new(new_instance)
         storage.save()
 
     def help_create(self):
