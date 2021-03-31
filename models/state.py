a68d7fbdf253fd4cf199e2a2a0c6705aa9334d8c
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-30 22:43:02 -0500

    [UPDATE] - Fix pep8 problem

--- models/state.py
+++ models/state.py
@@ -12,4 +12,3 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all,delete')
-
